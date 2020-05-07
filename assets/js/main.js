4f26f3c8112cdf9fc3a7f24ea50beb3ef6016068
sbirhan <sbirhan@gmail.com>
2020-05-07 23:07:35 +0200

    third commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 var berecehnung2020 = [9408, 14532, 57051, 270500, 972.87, 212.02, 972.79, 8963.74, 17078.74]; //first 4 value are income ranges
-var berecehnung2019 = [9168, 14254, 55960, 265326, 980.14, 216.16, 965.58, 8780.90, 16740,68]; //last 5 value are parameters in formulas
+var berecehnung2019 = [9168, 14254, 55960, 265326, 980.14, 216.16, 965.58, 8780.90, 16740.68]; //last 5 value are parameters in formulas
 var berecehnung2018 = [9000, 13996, 54949, 260532, 997.80, 220.13, 948.49, 8621.75, 16437.70];
 var berechnung = berecehnung2020;
 var income = 0;
